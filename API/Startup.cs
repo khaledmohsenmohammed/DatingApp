32ad350c702b8fe5a5cfeea889a446a8f0b3386e
Khaled Mohsen <50178461+khaledmohsenmohammed@users.noreply.github.com>
2021-01-09 00:58:18 +0200

    Entity AppUser

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,7 +39,6 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
 
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
